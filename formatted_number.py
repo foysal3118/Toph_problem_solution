f4a9d583b756a6457be1c874d4630975c6cbce7c
foysal3118 <foysaldurlov@gmail.com>
2023-10-28 15:21:58 +0600

    remove

--- formatted_number.py
+++
@@ -1,9 +0,0 @@
-n = input()
-m = str(n)
-nList = list(m)
-if len(m) <= 3:
-    print(n)
-elif len(m) % 3 == 0:
-    for i in range((len(m) // 3) + 1):
-        nList = []
-        finalOutput = ""
--- full_pyramid.py
+++
@@ -1,7 +0,0 @@
-n = int(input())
-for i in range(1, n + 1):
-    first_space = " " * (n - i)
-    print(first_space + "*", end="")
-    for j in range(i - 1):
-        print(" " + "*", end="")
-    print()
