2ca52b8ca6f04bba7813f0a576eccc281123c726
foysal3118 <foysaldurlov@gmail.com>
2024-02-12 02:35:09 +0600

    Almighty Push

--- Divisors.py
+++ Divisors.py
@@ -1,12 +1,4 @@
-n = int(input())
-divisor = []
-for i in range(1, n + 1):
-    if n % i == 0:
-        initial_div = n // i
-        if initial_div not in divisor:
-            divisor.append(initial_div)
-            if i not in divisor:
-                divisor.append(i)
-divisor.sort()
-for j in divisor:
-    print(j)
+Input = int(input())
+for i in range(1, Input + 1):
+    if Input % i == 0:
+        print(i)
