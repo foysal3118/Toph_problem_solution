e636a1d4399985db1a26589d3bde1be4da821b8a
foysal3118 <foysaldurlov@gmail.com>
2024-02-12 02:14:36 +0600

    Almighty Push

--- Clock_math.py
+++ Clock_math.py
@@ -1,9 +1,13 @@
-hr, min = map(int, input().split())
-hr_min = hr * 5
-min_multy = float(5 / 60)
-hr_to_12 = (60 - hr_min) - (min * min_multy)
-total_angle = (hr_to_12 + min) * 6
-if total_angle > 180:
-    print(360 - total_angle)
-else:
-    print(total_angle)
+hr, min = [int(i) for i in input().split()]
+# space_cal;
+min_space_from_12_to_CW = min
+hr_space_from_12_to_acw = 60 - (hr * 5)
+# 1 min = 5/60 space
+hr_space_decrese_to_min = hr_space_from_12_to_acw - (min * (5 / 60))
+# degree_cal;
+min_degree_from_12 = min_space_from_12_to_CW * 6  # 1 space = 6 degree
+hr_degree_from_12 = hr_space_decrese_to_min * 6
+total_degree = min_degree_from_12 + hr_degree_from_12
+if total_degree > 180:
+    total_degree = 360 - total_degree
+print(total_degree)
